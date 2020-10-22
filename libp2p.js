0907c0c9f8f1cd784d44be3609bea6dc3a0f7c18
DDATAA <guillaume.bouzige@ddataa.org>
2020-10-22 18:07:54 +0200

    mmmm

--- libp2p.js
+++ libp2p.js
@@ -20,7 +20,7 @@ const createLibp2p = async (peerId) => {
     addresses: {
       listen: [
         // Add the signaling server multiaddr
-        '/dns4/haciendas.ddataa.org/tcp/443/wss/p2p-webrtc-star/'
+        '/dns4/haciendas.chat.ddataa.org/tcp/443/wss/p2p-webrtc-star/'
       ]
     },
     modules: {
@@ -34,7 +34,7 @@ const createLibp2p = async (peerId) => {
     config: {
       peerDiscovery: {
         bootstrap: {
-          list: ['/dns4/haciendas.ddataa.org/tcp/443/wss/p2p-webrtc-star/p2p/QmWjz6xb8v9K4KnYEwP5Yk75k5mMBCehzWFLCvvQpYxF3d']
+          list: ['/dns4/haciendas.chat.ddataa.org/tcp/443/wss/p2p-webrtc-star/p2p/QmWjz6xb8v9K4KnYEwP5Yk75k5mMBCehzWFLCvvQpYxF3d']
         }
       },
       dht: {
